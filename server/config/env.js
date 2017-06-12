9c1f64dedd7895db863dd7134af70c32862ead13
Yogendra Singh <yog27ray@gmail.com>
2017-06-13 02:06:05 +0530

    setup file

--- server/config/env.js
+++ server/config/env.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 const log = debug('config/env');
 const root = path.normalize(__dirname + '/../..');
 const envFile = path.join(root, '.env');
-const setupCompleted = !fs.existsSync(envFile);
+const setupCompleted = fs.existsSync(envFile);
 const variables = setupCompleted ? dotenv.config({ path: envFile }) : {};
 const env = Object.assign({ setupCompleted, envFile, root }, variables.parsed || variables);
 
