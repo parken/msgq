0b98818b25b3a71e4330c1caec51052ecb3b19e2
Yogendra Singh <yog27ray@gmail.com>
2017-07-13 20:52:38 +0530

    export to excel for messages export to excel for sender id

--- server/api/senderId/index.js
+++ server/api/senderId/index.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/', oauth(), controller.create);
 router.get('/', oauth(), controller.index);
+router.get('/xls', controller.createXls);
 router.get('/:id', oauth(), controller.show);
 router.put('/:id/block', oauth(), controller.block);
 router.put('/:id/approve', oauth(), controller.approve);
